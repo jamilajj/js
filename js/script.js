c7e26345f2ac62035ad7c13a9590dfc86ba22142
Jamila J. Jumshudova <jamilajj@code.local>
2023-03-28 10:14:04 +0400

    musbet menfi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let input=prompt( "zehmet olmasa reqem daxil edin");
-let result =(input%2==0)? 'cutdur':'tekdir'
-alert(result);
+// let result = (input=="0") ? 'ne tekdir ne cut' :  (input%2==0) ? 'cutdur' :  'tekdir'
 
+let result=(input>0)? 'musbetdir': (input==0)? 'neytral' :'menfidir' 
+alert(result);
